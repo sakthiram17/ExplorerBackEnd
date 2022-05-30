5966cc70839f3b6e8186d1c366ba34fdad14fb71
Sakthi Ram <72368668+sakthiram17@users.noreply.github.com>
2022-05-30 17:16:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.obhkn.mongodb.net/?retryWrites=true&w=majority`
+    `mongodb+srv://sakthi:sakthi123@cluster0.obhkn.mongodb.net/?retryWrites=true&w=majority`
   )
   .then(() => {
     app.listen(process.env.PORT || 5000);
